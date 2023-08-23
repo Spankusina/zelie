7127c0f32ed3af15d276f920cd3633ba7c6c86f8
Spankusina <chaplinskiysv@gmail.com>
2023-08-23 23:38:56 +0700

    update control panel

--- static/js/appVue.js
+++ static/js/appVue.js
@@ -3,18 +3,18 @@ const App = {
         return {
             inputNameDisabled: false,
             listPlayers: [],
+            playersForExtraScore: [],
             inputNameValue: '',
+            inputScore: '',
             scoreOptions: [0, 1, 2, 3, 4, 6, 8, 10],
             isGameStarted: false,
+            isDisabledExtraScore: true,
             playerTurn: ''
         }
     },
     methods: {
-        inputNameChangeHandler(event) {
-            this.inputNameValue = event.target.value
-        },
         addNewPlayer() {
-            if (this.inputNameValue != ''){
+            if (this.inputNameValue != '' && !this.listPlayers.includes(this.inputNameValue)){
                 this.listPlayers.push(this.inputNameValue)
             }
             this.inputNameValue = ''
@@ -22,6 +22,21 @@ const App = {
         startGame() {
             this.playerTurn = this.listPlayers[0]
             this.isGameStarted = true
+        },
+        updateScore() {
+            const index = this.listPlayers.indexOf(this.playerTurn) < (this.listPlayers.length - 1) ? (this.listPlayers.indexOf(this.playerTurn) + 1) : 0
+            this.playerTurn = this.listPlayers[index]
+            this.inputScore = ''
+        }
+    },
+    watch: {
+        inputScore(value) {
+            if (value === '' || (value) < 4) {
+                this.isDisabledExtraScore = true
+            }
+            else {
+                this.isDisabledExtraScore = false
+            }
         }
     }
 }
