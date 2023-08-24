14babff7c27dac63a0d783ddf6f83cf1c4e6b277
Spankusina <chaplinskiysv@gmail.com>
2023-08-25 00:34:21 +0700

    implemented scoring

--- static/js/appVue.js
+++ static/js/appVue.js
@@ -1,38 +1,53 @@
 const App = {
     data() {
         return {
-            inputNameDisabled: false,
+            scoreOptions: [0, 1, 2, 3, 4, 6, 8, 10],
             listPlayers: [],
-            playersForExtraScore: [],
+            inputPlayersForExtraScore: [],
             inputNameValue: '',
             inputScore: '',
-            scoreOptions: [0, 1, 2, 3, 4, 6, 8, 10],
             isGameStarted: false,
             isDisabledExtraScore: true,
-            playerTurn: ''
+            indexPlayerTurn: 0
         }
     },
     methods: {
         addNewPlayer() {
-            if (this.inputNameValue != '' && !this.listPlayers.includes(this.inputNameValue)){
-                this.listPlayers.push(this.inputNameValue)
+            if (this.inputNameValue && !this.listPlayers.includes(this.inputNameValue)){
+                const newPlayer = {
+                    name: this.inputNameValue,
+                    totalScore: 0,
+                    stringScore: ''
+                }
+                this.listPlayers.push(newPlayer)
             }
             this.inputNameValue = ''
+            this.$refs.inputFocus.focus()
         },
         startGame() {
-            this.playerTurn = this.listPlayers[0]
             this.isGameStarted = true
         },
         updateScore() {
-            const index = this.listPlayers.indexOf(this.playerTurn) < (this.listPlayers.length - 1) ? (this.listPlayers.indexOf(this.playerTurn) + 1) : 0
-            this.playerTurn = this.listPlayers[index]
+            this.listPlayers[this.indexPlayerTurn].totalScore += this.inputScore
+            this.listPlayers[this.indexPlayerTurn].stringScore = this.listPlayers[this.indexPlayerTurn].stringScore ? this.listPlayers[this.indexPlayerTurn].stringScore + `, ${this.inputScore}` : this.inputScore
+            this.indexPlayerTurn = this.indexPlayerTurn < this.listPlayers.length - 1 ? this.indexPlayerTurn + 1 : 0
+
+            this.inputPlayersForExtraScore.forEach(index => {
+                this.listPlayers[index].totalScore += this.inputScore / 2
+                this.listPlayers[index].stringScore = this.listPlayers[index].stringScore ? this.listPlayers[index].stringScore + `, ${this.inputScore / 2}` : this.inputScore / 2
+            })
+            
             this.inputScore = ''
+            if (this.inputPlayersForExtraScore.length){
+                this.inputPlayersForExtraScore.splice(0, this.inputPlayersForExtraScore.length)
+            }
         }
     },
     watch: {
         inputScore(value) {
-            if (value === '' || (value) < 4) {
+            if (!value || (value) < 4) {
                 this.isDisabledExtraScore = true
+                this.inputPlayersForExtraScore.splice(0, this.inputPlayersForExtraScore.length)
             }
             else {
                 this.isDisabledExtraScore = false
